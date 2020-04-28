181dd3d7a70fa9682ade0cef12a4dd6a57852f6d
yamaimo <yarnaimo@gmail.com>
2020-04-28 18:07:56 +0900

    UnionToIntersection / PromiseReturnType

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,11 @@
 export type PlainObject = { [key: string]: any }
+
+export declare type UnionToIntersection<U> = (
+    U extends any ? (k: U) => void : never
+) extends (k: infer I) => void
+    ? I
+    : never
+
+export declare type PromiseReturnType<
+    F extends (...args: any) => Promise<any>
+> = F extends (...args: any) => Promise<infer T> ? T : never
